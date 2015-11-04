8c7fa7a8ee40a12fac1eb1e9dcbaeb6746654c6f
vampireliang <designereye@126.com>
2015-11-04 18:46:03 +0800

    zhf modified

--- Sources/js/app/Database.js
+++ Sources/js/app/Database.js
@@ -1,20 +1,38 @@
-define(['knockout','jquery','DBObj/date','DBObj/whenprinted','DBObj/tablelookup'],function (ko,$,date,whenprinted,tablelookup){
+define(['knockout','jquery','DBObj/date','DBObj/attr','DBObj/tablelookup'],function (ko,$,date,Attr,tablelookup){
 	var database = function (){
 		var self = this;
 		self.date = ko.observableArray();
 		self.whenprinted = ko.observableArray();
 		self.tablelookup = ko.observableArray();
+		// self.attr = ko.observable();
 
 		self.init = function (){
-			self.addDate()
-			self.addWhenprinted();
+			self.addDate();
+			self.initForm();
+			// self.addWhenprinted();
 			self.addTablelookup();
 		};
 		self.addDate = function (){
 			self.date.push(new date);
 		};
-		self.addWhenprinted = function (){
-			self.whenprinted.push(new whenprinted);
+		self.initForm = function(){
+			$(document).on('click','#addFormAttr',function(){
+				var name = $('#formname').val();
+				self.addAttr(name);
+			});
+			$(document).on('contextmenu','#formattr li',function(e){
+				e.preventDefault();
+				$(this).siblings('.rightmenu.menu').show().position({
+						my: "left top",
+						of: e
+					});
+			});
+		};
+		self.addAttr = function (name){
+			var attr = new Attr();
+			attr.name = name;
+			self.whenprinted.push(attr);
+			$("#biaodanshuru").dialog('close');
 		};
 		self.addTablelookup = function (){
 			self.tablelookup.push(new tablelookup);
@@ -22,6 +40,9 @@ define(['knockout','jquery','DBObj/date','DBObj/whenprinted','DBObj/tablelookup'
 		self.showAdddate = function (){
 			$("#riqi").dialog( "open" );
 		};
+		self.showFormAttr = function(){
+			console.log(this);
+		}
 		self.showAddwhenprinted = function (){
 			$("#biaodanshuru").dialog("open");
 		};
