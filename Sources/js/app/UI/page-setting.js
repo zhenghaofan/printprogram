c2720cd7d67c60ea7d4c3ae5bfe5a79b06aee5c9
zhenghaofan <357034385@qq.com>
2015-11-05 16:15:04 +0800

    sdf

--- Sources/js/app/UI/page-setting.js
+++ Sources/js/app/UI/page-setting.js
@@ -5,7 +5,7 @@ define(['jquery'], function($){
 	$(document).on('click','.form-setting',function(){
 		$('#form-setting-dialog').dialog("open");
 	});
-	$(document).on('click','.icon-table2',function(){
+	$(document).on('click','.icon-table2,.formtab',function(){
 		$('#form-dialog').dialog("open");
 	});
 	$(document).on('click','.exception',function(){
